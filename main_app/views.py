1393ceb86edca3de438f36bc9b4e437d12381eb2
jmolasky <jeromemolasky@gmail.com>
2022-03-07 00:26:04 -0600

    wip: adds basic authentication/authorization

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
+@login_required
 def home(request):
-    return HttpResponse('Hello World')
+    return render(request, 'home.html')
+
+@login_required
+def about(request):
+    return render(request, 'about.html')
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'invalid signup - please try again'
+    form = UserCreationForm()
+    context = {
+        'form': form,
+        'error': error_message,
+    }
+    return render(request, 'registration/signup.html', context)
