a418c21f7cd9925021f1c93c3197e3b668b491c1
jmolasky <jeromemolasky@gmail.com>
2022-03-09 17:47:36 -0600

    wip: registers Amount model in admin.py

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Crypto, Wallet
+from .models import Crypto, Wallet, Amount
 
 # Register your models here.
 
 admin.site.register(Crypto)
 admin.site.register(Wallet)
+admin.site.register(Amount)
--- main_app/urls.py
+++ main_app/urls.py
@@ -6,8 +6,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('accounts/signup/', views.signup, name='signup'),
 
-
-
 ]
 
 
