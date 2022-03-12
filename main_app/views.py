d9e1f61b91b826345743499b89dedfefa4c2ccdb
riobotic <mario.reynoso.ii@gmail.com>
2022-03-12 12:42:21 -0500

    updating urls/views

--- main_app/views.py
+++ main_app/views.py
@@ -31,3 +31,22 @@ def signup(request):
         'error': error_message,
     }
     return render(request, 'registration/signup.html', context)
+
+def wallets_index(request):
+    wallets = Wallet.objects.all()
+    return render(request, 'wallets/index.html', {'wallets': wallets})
+
+def wallets_detail(request, wallet_id):
+    wallet = Wallet.objects.get(id=wallet_id)
+    # query database for a specific wallet's coins
+    wallet_coins = Amount.objects.filter(wallet=wallet_id)
+    # TODO: use the API to pull additional data about each currency to display - will need to do some
+    # math etc. to get this to work but it should be doable
+   
+    coins_not_in_wallet = CryptoCurrency.objects.exclude(id__in=wallet_coins.values_list('crypto'))
+
+    return render(request, 'wallets/detail.html', {
+        'wallet': wallet,
+        'avail_coins': coins_not_in_wallet,
+        'coins': wallet_coins,
+    })
