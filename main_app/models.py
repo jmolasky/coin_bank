4dfe1d9ed5a2f409d91f74d66fdad77f067b46fe
riobotic <mario.reynoso.ii@gmail.com>
2022-03-19 10:27:43 -0400

    updating main app

--- main_app/models.py
+++ main_app/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from django.urls import reverse
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 
 # Create your models here.
 
 # just call this Crypto in the actual app
-class Crypto(models.Model):
+class CryptoCurrency(models.Model):
     symbol = models.CharField(max_length=100)
    
     def __str__(self):
@@ -13,16 +12,20 @@ class Crypto(models.Model):
 
 class Wallet(models.Model):
     name = models.CharField(max_length=100)
-    crypto = models.ManyToManyField(Crypto, through='Amount')
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # do we need this?
+    location = models.CharField(max_length=100)
+    # typo here but it's just a test so it's fine
+    crpto = models.ManyToManyField(CryptoCurrency, through='Amount')
+    # we'll have a user attribute for our actual app
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name
+        return f"{self.name} at {self.location}"
 
 class Amount(models.Model):
     wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE)
     # probably shouldn't even be able to delete crypto because it would remove it from each wallet it's in
-    crypto = models.ForeignKey(Crypto, on_delete=models.CASCADE)
+    crypto = models.ForeignKey(CryptoCurrency, on_delete=models.CASCADE)
     amount = models.FloatField()
 
     def __str__(self):
