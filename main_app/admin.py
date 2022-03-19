b21339508f0c1d667405ea71bea51c3f22f05725
riobotic <mario.reynoso.ii@gmail.com>
2022-03-19 10:42:20 -0400

    crypto

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import CryptoCurrency, Wallet, Amount
+from .models import Crypto, Wallet, Amount
 # Register your models here.
 
-admin.site.register(CryptoCurrency)
+admin.site.register(Crypto)
 admin.site.register(Wallet)
 admin.site.register(Amount)
