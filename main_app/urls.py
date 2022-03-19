e2711a677f73e409a1394fdf93e74a31fdf798ea
riobotic <mario.reynoso.ii@gmail.com>
2022-03-19 10:48:46 -0400

    URL updates

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('wallets/', views.wallets_index, name='dashboard'),
+    path('about/', views.about, name='about'),
+    path('accounts/signup/', views.signup, name='signup'),
+    path('wallets/', views.wallets, name='wallets'),
     path('wallets/<int:wallet_id>', views.wallets_detail, name='detail'),
-    path('wallets/create', views.add_wallet, name='add_wallet'),
+
 ]
