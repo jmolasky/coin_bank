b833bdd3bc9c6935077c3d18dabdd010a84ab326
jmolasky <jeromemolasky@gmail.com>
2022-03-19 10:21:32 -0500

    wip: add index, detail, and create views

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
     path('accounts/signup/', views.signup, name='signup'),
-    path('wallets/', views.wallets, name='wallets'),
+    path('wallets/', views.wallets_index, name='dashboard'),
     path('wallets/<int:wallet_id>', views.wallets_detail, name='detail'),
+    path('wallets/create', views.add_wallet, name='add_wallet'),
 
 ]
