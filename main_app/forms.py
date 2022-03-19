53d908a6309ae336cd239e8c8eab8fea3a3512bf
jmolasky <jeromemolasky@gmail.com>
2022-03-19 15:05:40 -0500

    wip: adds page for coin list/coin add functionality

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,8 +1,13 @@
 from django.forms import ModelForm
-from .models import Wallet
+from .models import Wallet, Crypto
 
 class WalletForm(ModelForm):
     # accesses object that created ModelForm class
     class Meta:
         model = Wallet
         fields = ('name',)
+
+class CoinForm(ModelForm):
+    class Meta:
+        model = Crypto
+        fields = ('symbol',)
