d6a6df40378a3eaacc0076ed1b83f23df6424048
jmolasky <jeromemolasky@gmail.com>
2022-03-19 12:29:56 -0500

    wip: adds Shin's changes

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/signup/', views.signup, name='signup'),
     path('wallets/', views.wallets_index, name='dashboard'),
+    # this is the path for the detail page, but the detail page is also the edit page for 
+    # the wallets
     path('wallets/<int:wallet_id>', views.wallets_detail, name='detail'),
     path('wallets/create', views.add_wallet, name='add_wallet'),
 
