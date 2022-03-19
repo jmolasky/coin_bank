a21b71cf98ce21ad3cc371fd92f804890fc2d641
sochi2122 <90932673+sochi2122@users.noreply.github.com>
2022-03-19 12:31:54 -0400

    ok

---
+++ main_app/serializer.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['url', 'name']
+
+
+
+# API Key for coin market cap : f1255f3a-72de-4c0f-8067-b6b1764c3cf0
