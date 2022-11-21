db07aa85b89bbd18f8562a5e8c717eff2261094c
jmolasky <jeromemolasky@gmail.com>
2022-11-21 17:03:53 -0600

    update csrf trusted origins

--- coin_bank/settings.py
+++ coin_bank/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-CSRF_TRUSTED_ORIGINS = ['https://coin-bank.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://coin-bank.up.railway.app']
 # Application definition
 
 INSTALLED_APPS = [
